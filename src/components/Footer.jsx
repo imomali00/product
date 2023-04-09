020a41a41c5b78cff1acf518af01c35683a29cbd
Macbook Air <macbookair@MacBook-Air-Macbook-2.local>
2023-04-09 15:20:00 +0500

    a

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 
@@ -5,7 +6,7 @@ const Footer = () => {
   return (
     <>
       <div className="flex w-full gap-2 h-auto">
-        <a
+        <Link
           href="/cart"
           className="bg-gray-200 w-[50%] h-14 rounded-2xl cursor-pointer"
         >
@@ -13,7 +14,7 @@ const Footer = () => {
             <FaShoppingCart />
             <p>Корзина</p>
           </div>
-        </a>
+        </Link>
         <a className="bg-gray-200 w-[50%] h-14 rounded-2xl cursor-pointer">
           <div className="flex gap-2 text-xl justify-center items-center h-full">
             <FaUser />
